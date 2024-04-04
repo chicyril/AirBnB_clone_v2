2235846f79cba601db47fc490ff69a6a265a43b3
cyril <cyrildaniels21+git@gmail.com>
2024-04-04 10:31:51 +0100

    add fabric (python) scripts for .tgz-archiving and deploying web-static to servers (1-pack_web_static.py and 2-do_deploy_web_static.py).

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive from the contents of the
+web_static folder of AirBnB Clone repo, using the function do_pack.
+"""
+from fabric.api import local
+from datetime import datetime
+from os.path import isdir, getsize
+from os import mkdir
+
+
+def do_pack():
+    """Fabric task for generating .tgz from the contents of web_static dir."""
+    if not isdir("versions"):
+        mkdir("versions")
+    date_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = f"versions/web_static_{date_str}.tgz"
+    print(f"Packing web_static to {archive_path}")
+    local(f"tar -czvf {archive_path} web_static/*")
+    size = getsize(archive_path)
+    print(f'web_static packed: {archive_path} -> {size}Bytes')
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Fabric script that distributes an archive to web-servers."""
+from fabric.api import run, put, env
+from os.path import exists, basename, splitext
+env.hosts = ['52.86.157.4', '100.25.14.58']
+
+
+def do_deploy(archive_path):
+    """Deploy an archive to web-servers."""
+    try:
+        if not exists(archive_path):
+            return False
+        archive = basename(archive_path)
+        dir_name = splitext(archive)[0]
+        remote_location = f'/data/web_static/releases/{dir_name}'
+        put(archive_path, f"/tmp/{archive}")
+        run(f'mkdir -p {remote_location}')
+        run(f'tar -xzf /tmp/{archive} -C {remote_location}')
+        run(f'mv {remote_location}/web_static/* {remote_location}')
+        run(f'rm -rf {remote_location}/web_static/')
+        run(f'rm /tmp/{archive}')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -s {remote_location} /data/web_static/current')
+        print('New version deployed!')
+    except Exception as e:
+        print('Error occured: ', e)
+        return False
+    return True
